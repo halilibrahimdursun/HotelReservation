57679e23cb2b5e58dbe064d0e198da89295cfacc
DMATSALY <dsms@ukr.net>
2023-06-12 20:17:01 +0200

    Thymeleaf added

--- src/main/java/com/application/mvc/MvcController.java
+++ src/main/java/com/application/mvc/MvcController.java
@@ -1,10 +1,110 @@
 package com.application.mvc;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MvcController {
+
+    // http://localhost:8080
+    @GetMapping("/")
+    public String slashPage() {
+
+        System.out.println("Inside homePage");
+
+        return "home";
+    }
+
+//    @GetMapping("/")
+//    public String redirectToIndex() {
+//        return "redirect:/index.html";
+//    }
+
+    @GetMapping("/index")
+    public String indexPage() {
+
+        System.out.println("Inside homePage");
+
+        return "redirect:/index.html";
+//        return "index";
+    }
+
+
+
+    @GetMapping("/home")
+    public String homePage() {
+
+        System.out.println("Inside homePage");
+
+        return "home";
+    }
+
+    // http://localhost:8080
+    @GetMapping("/login")
+    public String loginPage() {
+
+        System.out.println("Inside loginPage");
+
+        return "login";
+
+    }   // http://localhost:8080
+
+
+
+
+    // http://localhost:8080/customer
+    @GetMapping("/customer")
+    public String customerPage() {
+
+        System.out.println("Inside customerPage");
+
+        // use customer for normal use
+        // use customerT for 'template' use (header, menu, footer, etc.)
+        return "customer";
+    }
+
+    // http://localhost:8080/customer_no_thymeleaf
+    @GetMapping("/customer_no_thymeleaf")
+    public String customer_no_thymeleafPage() {
+
+        System.out.println("Inside customer_no_thymeleafPage");
+
+        // use customer for normal use
+        // use customerT for 'template' use (header, menu, footer, etc.)
+        return "customer_no_thymeleaf";
+    }
+
+    // http://localhost:8080/reservation
+    @GetMapping("/reservation")
+    public String reservationPage() {
+
+        System.out.println("Inside reservationPage");
+
+        return "reservation";
+    }
+
+    // http://localhost:8080/room
+    @GetMapping("/room")
+    public String roomPage() {
+
+        System.out.println("Inside roomPage");
+
+        return "room";
+    }
+}
+
+
+
+
+
+
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class MvcController {
 //
 //    // http://localhost:8080/customer
 //    @GetMapping("/")
@@ -26,14 +126,30 @@ public class MvcController {
 //        return "customerT";
 //    }
 //
-    // http://localhost:8080/reservation
-    @GetMapping("/reservation")
-    public String reservationPage() {
-
-        System.out.println("Inside reservationPage");
-
-        return "reservation";
-    }
-
-
-}
+//    // http://localhost:8080/reservation
+//    @GetMapping("/reservation")
+//    public String reservationPage() {
+//
+//        System.out.println("Inside reservationPage");
+//
+//        return "reservation";
+//    }
+//
+//    // http://localhost:8080/contact
+//    @GetMapping("/contact")
+//    public String contactPage() {
+//
+//        System.out.println("Inside cantactPage");
+//
+//        return "contact";
+//    }
+//
+//    // http://localhost:8080/blog
+//    @GetMapping("/blog")
+//    public String blogPage() {
+//
+//        System.out.println("Inside blogPage");
+//
+//        return "blog";
+//    }
+//}
